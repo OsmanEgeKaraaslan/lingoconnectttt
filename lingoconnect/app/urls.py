543772c509e4345b9f2e7b04937d885046edaeac
OmerFarukKocaa <omer.koca@live.acibadem.edu.tr>
2023-12-20 13:58:33 +0300

    newest version - css codes are added

--- lingoconnect/app/urls.py
+++ lingoconnect/app/urls.py
@@ -3,7 +3,7 @@ from . import views
 from django.contrib.auth.views import LoginView
 urlpatterns = [
     path('',views.home,name="home"),
-    path("quizes/",views.Quizes,name="quizes"),
+    path("quizzes/",views.Quizes,name="quizes"),
     path("Quiz/<int:quiz_id>/",views.quiz,name="Quiz"),
     path('submit-quiz/<int:quiz_id>/',views.submit_quiz,name='submit_quiz'),
     path('register/', views.register, name='register'),
